6ef79d29ec545fc910b24d54220831da86211803
AndreasHeindorff <heindorff94@gmail.com>
2017-10-04 08:51:41 +0200

    rulebase

---
+++ Rulebase/src/main/java/controller/BankController.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controller;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author Andreas
+ */
+public class BankController {
+    
+    private List<Bank> banks = new ArrayList<Bank>();
+    
+    public List<Bank> GetAcceptedBanks(int cs, int amount, int dur){
+        System.out.println("hello");
+        List<Bank> acceptedBanks = new ArrayList<Bank>();
+        for (int i = 0; i < banks.size(); i++) {
+           if(banks.get(i).IsRequirementMet(cs, i, i)) 
+               acceptedBanks.add(banks.get(i));
+        }
+        return acceptedBanks;
+    }
+
+    public BankController() {
+        Bank SOFT_XML_Bank = new Bank("SOFT_XML_Bank", 0, 1000, 10000, 90);
+        Bank SOFT_JSON_Bank = new Bank("SOFT_JSON_Bank", 200, 10000, 30000, 120);
+        Bank STUD_XML_Bank = new Bank("STUD_XML_Bank", 400, 20000, 50000, 180);
+        Bank STUD_JSON_Tank = new Bank("STUD_JSON_Tank", 600, 100000, 500000, 240);
+        
+        banks.add(SOFT_XML_Bank);
+        banks.add(SOFT_JSON_Bank);
+        banks.add(STUD_XML_Bank);
+        banks.add(STUD_JSON_Tank);
+    }
+    
+    
+    
+    
+}
--- target/generated-sources/jaxws-wsimport/org/bank/credit/web/service/package-info.java
+++
@@ -1,2 +0,0 @@
-@javax.xml.bind.annotation.XmlSchema(namespace = "http://service.web.credit.bank.org/")
-package org.bank.credit.web.service;
