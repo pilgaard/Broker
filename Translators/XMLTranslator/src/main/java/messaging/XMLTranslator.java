35fcf0097dda5e6868972ccac73e0e39ec71c5bb
AndreasHeindorff <heindorff94@gmail.com>
2017-10-23 12:51:04 +0200

    XMLTranslator

--- Translators/XMLTranslator/src/main/java/messaging/XMLTranslator.java
+++ Translators/XMLTranslator/src/main/java/messaging/XMLTranslator.java
@@ -13,6 +13,8 @@ import com.rabbitmq.client.Consumer;
 import com.rabbitmq.client.DefaultConsumer;
 import com.rabbitmq.client.Envelope;
 import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.concurrent.TimeoutException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -22,11 +24,11 @@ import org.json.JSONObject;
  *
  * @author Andreas
  */
-public class JSONTranslator {
+public class XMLTranslator {
 
     private static final String Normalizer_Queue = "Normalizer";
-    private static final String QUEUE_IN = "Translator_CPH_JSON";
-    private static final String QUEUE_OUT = "cphbusiness.bankJSON";
+    private static final String QUEUE_IN = "Translator_CPH_XML";
+    private static final String QUEUE_OUT = "cphbusiness.bankXML";
     private static final String HOST_NAME = "datdb.cphbusiness.dk";
 
     public static void main(String[] args) throws IOException, TimeoutException {
@@ -34,18 +36,24 @@ public class JSONTranslator {
     }
 
     private static String FormatMessage(String message) {
-        JSONObject jsonObj = new JSONObject(message);
-        String ssn = jsonObj.getString("ssn");
+        JSONObject json = new JSONObject(message);
+        String ssn = json.getString("ssn");
         String newssn = ssn.replace("-", "");
-        int duration = jsonObj.getInt("duration");
-        int amount = jsonObj.getInt("amount");
-        int cs = jsonObj.getInt("cs");
-        String json
-                = "{ \"ssn\":\"" + newssn + "\","
-                + "\"creditScore\":" + cs + ","
-                + "\"loanAmount\":" + amount + ","
-                + "\"loanDuration\":" + duration + "}";
-        return json;
+        int duration = json.getInt("duration");
+        int amount = json.getInt("amount");
+        int cs = json.getInt("cs");
+        Calendar calendar = Calendar.getInstance();
+        calendar.set(1970, 0, 1);
+        calendar.add(Calendar.MONTH, duration);
+        SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss z");
+        String newdate = format.format(calendar.getTime());
+        String xml = "<LoanRequest>\n"
+                + "   <ssn>" + newssn + "</ssn>\n"
+                + "   <creditScore>" + cs + "</creditScore>\n"
+                + "   <loanAmount>" + amount + "</loanAmount>\n"
+                + "   <loanDuration>" + newdate + "</loanDuration>\n"
+                + "</LoanRequest>";
+        return xml;
     }
 
     private static void recieve() throws IOException, TimeoutException {
@@ -53,18 +61,13 @@ public class JSONTranslator {
         factory.setHost(HOST_NAME);
         factory.setUsername("student");
         factory.setPassword("cph");
-        Connection connection = factory.newConnection();
-
+        Connection connection = factory.newConnection();       
         Channel sendChannel = connection.createChannel();
         sendChannel.exchangeDeclare(QUEUE_IN, "direct");
-
         Channel replyChannel = connection.createChannel();
-
         replyChannel.queueDeclare(Normalizer_Queue, true, false, false, null);
         String replyQueue = replyChannel.queueDeclare().getQueue();
-
-        replyChannel.queueBind(replyQueue, QUEUE_IN, "BankJSON");
-
+        replyChannel.queueBind(replyQueue, QUEUE_IN, "BankXML");
         Consumer consumer = new DefaultConsumer(replyChannel) {
             @Override
             public void handleDelivery(String consumerTag, Envelope envelope, AMQP.BasicProperties properties, byte[] body) throws IOException {
@@ -74,7 +77,7 @@ public class JSONTranslator {
                     send(FormatMessage(receivedMessage));
 
                 } catch (TimeoutException ex) {
-                    Logger.getLogger(JSONTranslator.class
+                    Logger.getLogger(XMLTranslator.class
                             .getName()).log(Level.SEVERE, null, ex);
                 }
             }
@@ -89,17 +92,13 @@ public class JSONTranslator {
         factory.setPassword("cph");
         Connection connection = factory.newConnection();
         Channel JSONChannel = connection.createChannel();
-
-        String replyKey = "jsonbank";
-
+        String replyKey = "xmlbank";
         JSONChannel.exchangeDeclare(QUEUE_OUT, "fanout");
-
         AMQP.BasicProperties basicProperties = new AMQP.BasicProperties()
                 .builder()
                 .replyTo(Normalizer_Queue)
-                .correlationId("BankJSON")
+                .correlationId("BankXML")
                 .build();
-
         System.out.println("Sent message: " + message);
         JSONChannel.basicPublish(QUEUE_OUT, replyKey, basicProperties, message.getBytes());
         JSONChannel.close();
